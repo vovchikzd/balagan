f21d1d1049dff6ad49f8ca9593d4f3721378ae1f
Vladimir Zdorenko <vovchikzd@gmail.com>
2023-12-20 02:56:22 +0300

    Rewrite main

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,25 @@ use std::borrow::Cow;
 const WIDTH: f32 = 960.;
 const HEIGHT: f32 = 540.;
 
-#[derive(Default)]
-struct MyEguiApp;
+struct Balagan;
 
-impl MyEguiApp {
-    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self::default()
-    }
-}
-
-impl eframe::App for MyEguiApp {
+impl eframe::App for Balagan {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui_extras::install_image_loaders(ctx);
         egui::CentralPanel::default().show(ctx, |ui| {
             let image = ImageSource::Uri(Cow::from("file:///home/vovchik/rust/balagan/test.jpg"));
             ui.image(image);
-            // ui.heading("Hello World!");
-            // ui.image(my_texture_id, [WIDTH, HEIGHT]);
         });
     }
 }
 
 fn main() {
-    let mut native_options = eframe::NativeOptions::default();
-    native_options.viewport.inner_size = Some(Vec2::new(WIDTH, HEIGHT));
-    native_options.viewport.resizable = Some(true);
+    let mut nat_opt = eframe::NativeOptions::default();
+    nat_opt.viewport.inner_size = Some(Vec2::new(WIDTH, HEIGHT));
+    nat_opt.viewport.resizable = Some(true);
     let _ = eframe::run_native(
         "balagan",
-        native_options,
-        Box::new(|cc| Box::new(MyEguiApp::new(cc))),
+        nat_opt,
+        Box::new(|_cc| Box::new(Balagan))
     );
 }
