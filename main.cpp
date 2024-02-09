ea79cf573506e7f28eb617aef7d35224ec142022
Vladimir Zdorenko <vovchikzd@gmail.com>
2024-02-09 17:23:04 +0300

    i can create window

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
-#include <iostream>
-#include <optional>
-#include <expected>
-#include <vector>
-#include <iterator>
+#include <QLabel>
+#include <QMouseEvent>
+#include <QtWidgets>
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args(argv, argv + argc);
-  std::ostream_iterator<std::string> osit(std::cout, " ");
-  std::copy(args.begin(), args.end(), osit);
-  return 0;
+  QApplication app(argc, argv);
+  QWidget window;
+  QLabel* label = new QLabel(&window);
+  window.setWindowTitle("First window");
+  window.resize(960, 540);
+  window.showMaximized();
+  QString str =
+      "This is have to long text, so I trying to make it more ane more "
+      "bigger.\nEven more, and again. Probably and again.\nAng over, and over "
+      "again.";
+  label->setAlignment(Qt::AlignLeft);
+  label->setText(str);
+  label->adjustSize();
+  window.show();
+  return app.exec();
 }
